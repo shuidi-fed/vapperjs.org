1304cb71d5f871abf59718622e0335d1a3671c0b
HcySunYang <HcySunYang@outlook.com>
2019-08-08 23:21:30 +0800

    Modify the deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,6 @@ git config user.email 'HcySunYang@outlook.com'
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:HcySunYang/vue-design.git master:gh-pages
+git push -f git@github.com:vapperjs/vapperjs.org.git master:gh-pages
 
 cd -
