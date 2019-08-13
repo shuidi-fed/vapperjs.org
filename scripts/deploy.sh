01dac68f0413baa89024ece1ec8c99ac9b6cf6e3
HcySunYang <HcySunYang@outlook.com>
2019-08-13 20:13:24 +0800

    custom domain

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,8 @@ npm run build
 
 cd docs/.vuepress/dist
 
+echo 'vapperjs.org' > CNAME
+
 git init
 git config user.name 'HcySunYang'
 git config user.email 'HcySunYang@outlook.com'
@@ -15,3 +17,7 @@ git commit -m 'deploy'
 git push -f git@github.com:vapperjs/vapperjs.org.git master:gh-pages
 
 cd -
+
+pwd
+
+rm -rf docs/.vuepress/dist
